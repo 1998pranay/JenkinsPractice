409179dcb583843747982a09b18e7a4ab6a251f7
Pranay Nimunkar <nimunkarpranay12345@gmail.com>
2024-04-05 08:16:20 +0530

    first commit

---
+++ SampleMavenOne/src/main/java/com/learn/jenkins/samplemavenone/SampleMavenOne.java
@@ -0,0 +1,11 @@
+
+
+package com.learn.jenkins.samplemavenone;
+
+
+public class SampleMavenOne {
+
+    public static void main(String[] args) {
+        System.out.println("First Sample Maven Project For Jenkins Practice");
+    }
+}
---
+++ SampleMavenTwo/src/main/java/com/learn/jenkins/samplemaventwo/SampleMavenTwo.java
@@ -0,0 +1,11 @@
+
+
+package com.learn.jenkins.samplemaventwo;
+
+
+public class SampleMavenTwo {
+
+    public static void main(String[] args) {
+        System.out.println("Second Sample Maven Project For Jenkins Practice");
+    }
+}
